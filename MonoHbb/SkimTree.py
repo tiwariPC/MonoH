7ad058f7fe2c3bcf39d4eec64b43e964b848f81b
Raman Khurana <raman.khurana@cern.ch>
2016-11-22 04:45:39 -0600

    rateparam.txt

--- MonoHbb/SkimTree.py
+++ MonoHbb/SkimTree.py
@@ -10,13 +10,13 @@ import numpy as numpy_
 ROOT.gROOT.LoadMacro("Loader.h+")
 
 ## When not running on farmout
-inputfilename= 'zj100.txt'
-outfilename= 'tmp.root'
+#inputfilename= 'MZp1000Ma0300.txt'
+#outfilename= 'MZp1000Ma0300_Puppi.root'
 
 
 ## When running on farmout
-#inputfilename = os.environ['INPUT']                                                                                                                                                 
-#outfilename   = os.environ['OUTPUT']                                                                                                                                                
+inputfilename = os.environ['INPUT']                                                                                                                                                 
+outfilename   = os.environ['OUTPUT']                                                                                                                                                
 
 
 skimmedTree = TChain("tree/treeMaker")
