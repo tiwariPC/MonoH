76f20201744996d93f556070a31b0fbfd32e5b28
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2017-09-12 17:34:32 +0530

    updates

--- MonoHbb/MonoHbbQuantities.py
+++ MonoHbb/MonoHbbQuantities.py
@@ -131,8 +131,8 @@ class MonoHbbQuantities:
         h_met_pdf_tmp = []
         for ipdf in range(101):
             midname = str(ipdf)
-            self.h_met_pdf.append(TH1F('h_met_pdf'+'_'+midname+'_',  'h_met_pdf',  1000,0.,1000.))
-        #self.h_met_pdf.append(h_met_pdf_tmp)
+            h_met_pdf_tmp.append(TH1F('h_met_pdf'+'_'+midname+'_',  'h_met_pdf',  1000,0.,1000.))
+        self.h_met_pdf.append(h_met_pdf_tmp)
         h_met_muR_tmp = []
         for imuR in range(2):
             midname = str(imuR)
@@ -153,7 +153,7 @@ class MonoHbbQuantities:
         
         
         for ipdf in range(101):
-            self.h_met_pdf        [ipdf].Fill(self.met,       1.0)
+            self.h_met_pdf[0]        [ipdf].Fill(self.met,       1.0)
 
         for imuR in range(2):
             self.h_met_muR[0]        [imuR].Fill(self.met,       1.0)
@@ -204,7 +204,7 @@ class MonoHbbQuantities:
         self.h_met[0].Write()
         #self.h_met_rebin[iregime].Write()
         for ipdf in range(101):
-            self.h_met_pdf[ipdf].Write()
+            self.h_met_pdf[0][ipdf].Write()
         for imuR in range(2):
             self.h_met_muR[0][imuR].Write()
         for imuF in range(2):
