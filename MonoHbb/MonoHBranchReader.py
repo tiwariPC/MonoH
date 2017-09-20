69f09a5696ea52e3a0c56fc722ade672b742014b
Praveen Chandra Tiwari <tiwariPC@users.noreply.github.com>
2017-09-20 14:58:44 +0530

    branch and tree

--- MonoHbb/MonoHBranchReader.py
+++ MonoHbb/MonoHBranchReader.py
@@ -205,8 +205,9 @@ def AnalyzeDataSet():
     allquantities = MonoHbbQuantities(outfilename)
     allquantities.defineHisto()
 
-    for x in allquantities if isinstance(x, float):
-       bbMET_tree.Branch('bbMETvariables',AddressOf(allquantities,'histo'),'histo/D')
+    for x in allquantities:
+       if isinstance(x, float):
+          bbMET_tree.Branch('bbMETvariables',AddressOf(allquantities,'histo'),'histo/D')
     # ----------------------------------------------------------------------------------------------------------------------------------------------------------------
     # ----------------------------------------------------------------------------------------------------------------------------------------------------------------
     # BTag Scale Factor Initialisation--------------------------------------------------------------------------------------------------------------------------------
