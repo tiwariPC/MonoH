b8f4fb333743c9a9e7bbac4825175ba4b13a3811
mondalspandan <mondalspandan@gmail.com>
2017-09-25 00:36:41 +0530

    Fixed bugs, added CR Table and Histo

--- MonoHbb/SkimTree.py
+++ MonoHbb/SkimTree.py
@@ -11,7 +11,7 @@ ROOT.gROOT.LoadMacro("Loader.h+")
 
 ## When not running on farmout
 inputfilename= 'FileList.txt'
-outfilename= 'Output_WJetsToLNu_HT-1200To2500.root'
+outfilename= 'Output_ZJetsToNuNu_HT-1200To2500.root'
 PUPPI = True
 CA15  = False
 
@@ -408,6 +408,8 @@ def AnalyzeDataSet():
         ZmumuMass[0] = -999.
         ZmumuRecoil[0] = -999.
         
+        TOPRecoil[0] = -999.
+        
         ## for dielectron 
         if len(myEles) ==2:
             ele1 = myEles[0]
@@ -470,7 +472,7 @@ def AnalyzeDataSet():
            
            
         ## for Single electron && Single Muon
-        if len(myEles) == 1 && len(myMuos) == 1:
+        if len(myEles) == 1 and len(myMuos) == 1:
            ele1 = myEles[0]
            p4_ele1 = eleP4[ele1]
            mu1 = myMuos[0]
