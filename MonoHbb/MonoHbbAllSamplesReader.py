548424fb8168eb7a5df1cbb3eab9aba6299d02bd
mondalspandan <mondalspandan@gmail.com>
2017-10-04 16:58:02 +0530

    Added parallelization to AllSamplesReader script and other minor changes, added list of test samples

--- MonoHbb/MonoHbbAllSamplesReader.py
+++ MonoHbb/MonoHbbAllSamplesReader.py
@@ -1,8 +1,14 @@
 import os 
 
 f = open('allfiles.txt','r')
+outdir = "bbMETSamples"
+
+os.system("mkdir -p "+outdir+"/log")
 
 for ifile in f: 
     filename =  ifile.rstrip()
+    print "\n=======================\nSubmitting",filename,"\n=======================\n"
     #os.system('python MonoHBranchReader.py -i '+filename+' -a')
-    os.system('python MonoHBranchReader.py -a -i '+filename+' -o ifile.split()[1].root -D bbMETSamples')
+    command='python MonoHBranchReader.py -a -i '+filename+' -D '+outdir+' &> '+outdir+'/log/'+filename.split('/')[-1].split('.')[0]+'.log &'
+    print "Executing shell command:",command
+    os.system(command)
